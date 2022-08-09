df6fd75e42f5d7d1c020602137012d18f5963ed2
Martin Odstrcilik <martin.odstrcilik@gmail.com>
2022-08-09 11:03:39 +0200

    feat: bump helm chart version to 0.1.2

--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ variable "helm_chart_name" {
 
 variable "helm_chart_version" {
   type        = string
-  default     = "0.1.1"
+  default     = "0.1.2"
   description = "Version of the Helm chart"
 }
 
